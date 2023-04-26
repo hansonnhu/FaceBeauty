f4dd81d260bdd43485e36c93a0d03229f6847ac8
Hu <aa254412@gmail.com>
2023-04-26 15:04:17 +0800

    splashScreen modify

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'login.dart';
 import 'home.dart';
 import 'dart:async';
+import 'splashScreen.dart';
 
 List<CameraDescription> cameras = [];
 Future<void> main() async {
@@ -20,138 +21,134 @@ Future<void> main() async {
   ]);
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false, // 移除 DEBUG 圖示
-    home: MyApp(), //這裡切換頁面(預設為MyApp)
-    // home: Home(),  //這裡直接跳至home page
+    home: SplashScreen(),  // 先跳轉至 SplashScreen page，再跳轉至 Login page
+    // home: MyApp(), //這裡切換頁面(預設為MyApp)
     // home: Login(),
   ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    bool loadingOK = false;
-    // _counting(){
-    //   print('counting');
-    //   Future.delayed(Duration(seconds: 5),);
-    //   loadingOK = true;
-    // }
-    return FutureBuilder(
-        future: Future.delayed(const Duration(seconds: 4), () {
-          loadingOK = true;
-        }),
-        builder: (context, AsyncSnapshot snapshot) {
-          // Loading
-          if (loadingOK == false) {
-            return const MaterialApp(
-              debugShowCheckedModeBanner: false,
-              home: SplashScreen(),
-            );
-          } else {
-            return GestureDetector(
-              onTap: () {
-                hideKeyboard(context);
-              },
-              child: const MaterialApp(
-                debugShowCheckedModeBanner: false,
-                home: Login(),
-              ),
-            );
-          }
-        });
-  }
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     bool loadingOK = false;
 
-  void hideKeyboard(BuildContext context) {
-    FocusScopeNode currentFocus = FocusScope.of(context);
-    if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
-      FocusManager.instance.primaryFocus?.unfocus();
-    }
-  }
-}
+//     return FutureBuilder(
+//         future: Future.delayed(const Duration(seconds: 3), () {
+//           loadingOK = true;
+//         }),
+//         builder: (context, AsyncSnapshot snapshot) {
+//           // Loading
+//           if (loadingOK == false) {
+//             return const MaterialApp(
+//               debugShowCheckedModeBanner: false,
+//               home: SplashScreen(),
+//             );
+//           } else {
+//             return GestureDetector(
+//               onTap: () {
+//                 hideKeyboard(context);
+//               },
+//               child: const MaterialApp(
+//                 debugShowCheckedModeBanner: false,
+//                 home: Login(),
+//               ),
+//             );
+//           }
+//         });
+//   }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+//   void hideKeyboard(BuildContext context) {
+//     FocusScopeNode currentFocus = FocusScope.of(context);
+//     if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
+//       FocusManager.instance.primaryFocus?.unfocus();
+//     }
+//   }
+// }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+//   // This widget is the home page of your application. It is stateful, meaning
+//   // that it has a State object (defined below) that contains fields that affect
+//   // how it looks.
 
-  final String title;
+//   // This class is the configuration for the state. It holds the values (in this
+//   // case the title) provided by the parent (in this case the App widget) and
+//   // used by the build method of the State. Fields in a Widget subclass are
+//   // always marked "final".
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+//   final String title;
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+//   void _incrementCounter() {
+//     setState(() {
+//       // This call to setState tells the Flutter framework that something has
+//       // changed in this State, which causes it to rerun the build method below
+//       // so that the display can reflect the updated values. If we changed
+//       // _counter without calling setState(), then the build method would not be
+//       // called again, and so nothing would appear to happen.
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // This method is rerun every time setState is called, for instance as done
+//     // by the _incrementCounter method above.
+//     //
+//     // The Flutter framework has been optimized to make rerunning build methods
+//     // fast, so that you can just rebuild anything that needs updating rather
+//     // than having to individually change instances of widgets.
+//     return Scaffold(
+//       appBar: AppBar(
+//         // Here we take the value from the MyHomePage object that was created by
+//         // the App.build method, and use it to set our appbar title.
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         // Center is a layout widget. It takes a single child and positions it
+//         // in the middle of the parent.
+//         child: Column(
+//           // Column is also a layout widget. It takes a list of children and
+//           // arranges them vertically. By default, it sizes itself to fit its
+//           // children horizontally, and tries to be as tall as its parent.
+//           //
+//           // Invoke "debug painting" (press "p" in the console, choose the
+//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
+//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+//           // to see the wireframe for each widget.
+//           //
+//           // Column has various properties to control how it sizes itself and
+//           // how it positions its children. Here we use mainAxisAlignment to
+//           // center the children vertically; the main axis here is the vertical
+//           // axis because Columns are vertical (the cross axis would be
+//           // horizontal).
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'dart:async';
+import 'login.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -7,11 +8,92 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+  late AnimationController _opacityController;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _opacityController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1500),
+    );
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _opacityController,
+        curve: Curves.easeInOut,
+      ),
+    );
+    _opacityController.repeat(reverse: true);
+
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      // Navigator.push(
+      //   context,
+      //   MaterialPageRoute(
+      //     builder: (_) => Login(),
+      //     maintainState: false,
+          
+      //   ),
+      // );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => Login()),
+      ).then((value) => setState(() {
+        // Do any initialization here
+        initState();
+      }));
+    });
+  }
+
+  @override
+  void dispose() {
+    _opacityController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width; //抓取螢幕寬度
+    double screenHeight = MediaQuery.of(context).size.height; //抓取螢幕高度
+
     return Center(
-      child: Center(child: Image.asset('assets/iconv4.gif')),
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.black,
+        child: Column(
+          children: [
+            Expanded(flex: 3, child: Container()),
+            FadeTransition(
+              opacity: _opacityAnimation,
+              child: Container(
+                child: Image.asset(
+                  'assets/iconv3.png',
+                  width: screenWidth / 2,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+            FadeTransition(
+              opacity: _opacityAnimation,
+              child: Container(
+                child: const Text(
+                  '傾國',
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+            ),
+            
+            Expanded(flex: 3, child: Container()),
+          ],
+        ),
+      ),
     );
   }
 }
